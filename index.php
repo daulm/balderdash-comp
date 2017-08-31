b1908a297187bbccf07529389e33361354c00e3f
daulm <dominick.lacoste@gmail.com>
2017-08-30 20:11:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ session_start();
 <title>Balderdash</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <script src="js/jquery.min.js"></script>
 <script type="text/javascript">
 
