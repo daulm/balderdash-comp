c563f5a38bc34739a02478cda907a57a00379ef9
daulm <dominick.lacoste@gmail.com>
2017-08-30 20:21:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ session_start();
 <head>
 <title>Balderdash</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <script src="js/jquery.min.js"></script>
