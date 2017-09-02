e69a2887a9550bde2d7e527b04f1220a6f5c2032
daulm <dominick.lacoste@gmail.com>
2017-09-02 14:02:58 -0400

    Update answer.php

--- answer.php
+++ answer.php
@@ -14,18 +14,50 @@ $con = mysqli_connect($db_host, $db_username, $db_pw, 'balderdash');
 if (!$con){
 	die('DB connection failed: '.mysqli_error($con));
 }
-
+	
 //Look up the clue and time limit
-$sql = "SELECT Clue, AnswerTime FROM lobby";
+$sql = "SELECT Clue, AnswerTime, DasherID FROM lobby";
 $sql .= " WHERE LobbyID=".$_SESSION['Lobby_ID'];
 if(!$result = mysqli_query($con, $sql)){
 	echo('Cant find code for this lobby');
 }	
 while($row = mysqli_fetch_row($result)){
 	$clue = $row[0];
-	$timeleft = $row[1];
+	$timeleft = $row[1]*60;
+	$dasherid = $row[2];
 }
 
+//if host, set up new game and change game state of lobby
+if(isset($_SESSION['Host'])){
+	//create new game 
+	$sql = "INSERT INTO games (LobbyID, Clue, DasherID) VALUES (";
+	$sql .= $_SESSION['Lobby_ID'].", '".$clue."', ".$dasherid.")";
+	if(!mysqli_query($con, $sql)){
+		echo('Unable to create Game');
+	}
+	$sql = "SELECT MAX(GameID) FROM games";
+	if(!$result = mysqli_query($con, $sql)){
+		echo('Unable to find newest Game');
+	}
+	$row = mysqli_fetch_row($result);
+	$_SESSION['Game_ID'] = $row[0];
+	
+	$sql = "UPDATE lobby SET GameID=".$_SESSION['Game_ID'].", GameState='answer' WHERE LobbyID=".$_SESSION['Lobby_ID'];
+	if(!mysqli_query($con, $sql)){
+		echo('Unable to sync Game to Lobby');
+	}
+}
+
+//set the gameid to the session
+$sql = "SELECT GameID FROM lobby";
+$sql .= " WHERE LobbyID=".$_SESSION['Lobby_ID'];
+if(!$result = mysqli_query($con, $sql)){
+	echo('Cant find code for this lobby');
+}	
+while($row = mysqli_fetch_row($result)){
+	$_SESSION['Game_ID'] = $row[0];
+}
+	
 mysqli_close($con);
 ?>
   
